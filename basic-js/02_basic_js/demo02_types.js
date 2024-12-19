e2552e3f3687abb24934b6cfddd8fe3f083915e5
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-12-19 18:02:11 +0530

    day#2 update#1

---
+++ basic-js/02_basic_js/demo02_types.js
@@ -0,0 +1,25 @@
+
+
+let a=20;
+console.log(typeof(a),a);
+
+let b=20.5;
+console.log(typeof(b),b);
+
+b="x";
+console.log(typeof(b),b);
+
+b='Hello World';
+console.log(typeof(b),b);
+
+b=`Mahaveer Ranches,
+Hosa Road`;
+
+let x=20;
+let y=30;
+
+let output=`sum of ${x} and ${y} is ${x+y}`;
+console.log(output);
+
+
+console.log(typeof(b),b);
