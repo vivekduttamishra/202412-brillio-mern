90e6c4b0b6b8008baeeb77544786fe9cbdb5b4f5
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-12-30 13:06:34 +0530

    day#6 update#2

--- basic-js/calculator_app/calc2_1.js
+++ basic-js/calculator_app/calc2_1.js
@@ -16,7 +16,9 @@ function infix(value1,operator,value2,result){
 
 const methodStyle=(value1,operator,value2,result)=>`${operator}( ${value1}, ${value2}) => ${result}`;
 
-let presenter = console.log
+const raw=(value1,operator,value2,result)=>result;
+
+let presenter = console.log;
 
 function calculator(value1, operator, value2, formatter='infix') {
     var result=operator(value1,value2);   
@@ -25,20 +27,32 @@ function calculator(value1, operator, value2, formatter='infix') {
         output=infix(value1,operator.name, value2, result);
     }else if(formatter==='methodStyle')
         output=methodStyle(value1,operator.name, value2, result);
+    else if(formatter==='raw')
+        output=raw(value1,operator.name, value2, result);
+    
 
     presenter(output);
 }
 
 
-//client code A. for calculator user.
+//CONSOLE CLIENT
+//The below code shouldn't run in browser apps
 
+try{
+    //if next line works
+    document.write('');
+    //we will not run our code.
+}catch(e){
+    
+    
+    calculator(10, plus, 5); //10 plus 5 = 15
+    calculator(10, minus, 5); //10 minus 5 = 5
+    
+    function mod(x,y) {return x%y;}
+    
+    calculator(10, mod, 5); //10 mod 5 = 0
+    
+    calculator(10, mod, 3, 'methodStyle');
 
+}
 
-calculator(10, plus, 5); //10 plus 5 = 15
-calculator(10, minus, 5); //10 minus 5 = 5
-
-function mod(x,y) {return x%y;}
-
-calculator(10, mod, 5); //10 mod 5 = 0
-
-calculator(10, mod, 3, 'methodStyle');
