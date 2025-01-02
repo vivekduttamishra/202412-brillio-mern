c7780d100ad00ec94b8e9e24f608d82b681f5cb9
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2025-01-02 12:42:20 +0530

    day#8 update#1

--- books_app_v2/app.js
+++ books_app_v2/app.js
@@ -1,30 +1,123 @@
 
-function getReviewList(reviews){
-    
-    let list="<ul>";
+function getReviewList(reviews) {
 
-    for(let review of reviews){
-        list+=`<li>${review.title}</li>`;
+    let list = "<ul>";
+
+    for (let review of reviews) {
+        list += `<li>${review.title}</li>`;
     }
 
-    list+="</ul>";
+    list += "</ul>";
     return list;
 }
 
-function sortOnPrice(){
-    console.log("Sort on Price");
-}
-function sortOnRating(){
-    console.log("Sort on Rating");
-}
-let searchTextBox=document.getElementById("search");
-let criteria = document.getElementById("criteria");
-function search(){
-
-    console.log("Searching", criteria.value, searchTextBox.value);
+function getAllBooks(){
+    displayBooks = books;
+    showBookCards(displayBooks);
 }
 
-function showBookCards(books){
+function sortOnPrice() {
+
+    let books=displayBooks;
+    //let sorted;
+    let size = books.length;
+
+    do {
+        var sorted = true; //optimisitic assumption that the  list is sorted
+        for (let i = 0; i < size - 1; i++) {
+            if (books[i].price > books[i+1].price) {
+                let temp = books[i];
+                books[i] = books[i + 1];
+                books[i + 1] = temp;
+                sorted = false; //oh! my assumption was wrong
+            }
+        }
+        size--;
+    } while (!sorted);
+    //console.log(books);
+    showBookCards(books);
+}
+
+
+function sortOnRating() {
+    let size = books.length;
+    let books=displayBooks;
+    do {
+        var sorted = true; //optimisitic assumption that the  list is sorted
+        for (let i = 0; i < size - 1; i++) {
+            if (books[i].rating < books[i+1].rating) {
+                let temp = books[i];
+                books[i] = books[i + 1];
+                books[i + 1] = temp;
+                sorted = false; //oh! my assumption was wrong
+            }
+        }
+        size--;
+    } while (!sorted);
+    //console.log(books);
+    showBookCards(books);
+}
+
+function sortOnAuthor(author){
+    let books=displayBooks;
+    let size = books.length;
+
+    do {
+        var sorted = true; //optimisitic assumption that the  list is sorted
+        for (let i = 0; i < size - 1; i++) {
+            if (books[i].author.toLowerCase() > books[i+1].author.toLowerCase()) {
+                let temp = books[i];
+                books[i] = books[i + 1];
+                books[i + 1] = temp;
+                sorted = false; //oh! my assumption was wrong
+            }
+        }
+        size--;
+    } while (!sorted);
+    showBookCards(books);
+}
+
+let searchTextBox = document.getElementById("search");
+let criteriaList = document.getElementById("criteria");
+function search() {
+    let criteria=criteriaList.value;
+    let query=searchTextBox.value;
+    console.log('criteria',criteria);
+    console.log('query',query);
+    query=query.toLowerCase();
+    let result=[];
+
+    for(let book of displayBooks){
+        if(criteria=='Title' && book.title.toLowerCase().includes(query))
+            result.push(book);
+        else if(criteria=='Author' && book.author.toLowerCase().includes(query))
+            result.push(book);
+        else if(criteria=='Price Range'){
+            let range= query.split('-');
+            let min = +range[0];
+            let max = +range[1];
+            if(book.price>=min && book.price<max){
+                result.push(book);
+            }
+        }
+        else if (criteria=='Rating Range'){
+            let range= query.split('-');
+            let min = +range[0];
+            let max = +range[1];
+            if(book.rating>=min && book.rating<max){
+                result.push(book);
+            }
+        }
+    }
+
+    displayBooks=result;
+    showBookCards(displayBooks);
+    
+
+
+}
+
+function showBookCards(books) {
 
     //console.log(`I will display given ${books.length} books`);
 
@@ -32,7 +125,7 @@ function showBookCards(books){
     //pageTitle.innerHTML=`Our Recommendation of ${books.length} Books`;
 
     let bookCount = document.getElementById('bookCount');
-    bookCount.innerHTML=books.length;
+    bookCount.innerHTML = books.length;
 
     // for(let i=0;i<books.length;i++){
     //     let cardId=`#card-${i+1}`; 
@@ -40,15 +133,15 @@ function showBookCards(books){
     //     document.querySelector(`${cardId} .title`).innerHTML=books[i].title;
     //     document.querySelector(`${cardId} img`).setAttribute('src',books[i].cover);
     //     document.querySelector(`${cardId} .subtitle`).innerHTML=books[i].author;
-    
+
     // }
-    
-    let booksDiv=document.getElementById('books');
 
-    let cards='';
+    let booksDiv = document.getElementById('books');
 
-    for(let book of books){
-        let card =`
+    let cards = '';
+
+    for (let book of books) {
+        let card = `
         <div class="card" id="${book.id}">
                 <img 
                     id="image-1"
@@ -65,23 +158,23 @@ function showBookCards(books){
                 ${getReviewList(book.reviews)}
         </div>        
         `
-        cards+=card;
+        cards += card;
     }
     booksDiv.innerHTML = cards;
 
 }
-function showBookTable(books){
+function showBookTable(books) {
 
-   
+
     let bookCount = document.getElementById('bookCount');
-    bookCount.innerHTML=books.length;
- 
-    let booksDiv=document.getElementById('books');
+    bookCount.innerHTML = books.length;
 
-    let table='';
+    let booksDiv = document.getElementById('books');
 
-    for(let book of books){
-        let bookTr =`
+    let table = '';
+
+    for (let book of books) {
+        let bookTr = `
         <tr  id="${book.id}">
         <td>
                 <img 
@@ -100,16 +193,18 @@ function showBookTable(books){
                 <td><a class="action-link" href="book1.html">Details</a></td>
         </tr>        
         `
-        table+=bookTr;
+        table += bookTr;
     }
-    document.getElementById('booksTableBody').innerHTML=table;
+    document.getElementById('booksTableBody').innerHTML = table;
 
 }
 
-(function main(){
+var displayBooks=books;
+
+(function main() {
     //checkLogin();
     //createNavBar();
-    showBookCards(books);
+    showBookCards(displayBooks);
     //showBookTable(books);
 })();
 
