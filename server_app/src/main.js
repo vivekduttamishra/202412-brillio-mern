8f40a61d3b470f42f40022ea1d65672525468744
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2025-01-17 12:45:04 +0530

    day#17 update#3

--- server_app/src/main.js
+++ server_app/src/main.js
@@ -8,10 +8,22 @@ function requestHandler(request,response){
 
 const server= http.createServer(requestHandler);
 
-const port = 8000;
 
-server.on('error', error=>console.error(`Server error: ${error.message}`));
 
-server.listen(port, success=>{
-    console.log(`Server running at http://localhost:${port}`);
-})
+
+//const port = process.env.PORT || 8000;
+
+let port = process.argv.pop() || 8000;
+port =parseInt(port);
+server.on('error',error=>{
+    port += 1;
+    startServer(port);
+});
+
+const startServer= port =>{
+    server.listen(port, success=>{
+        console.log(`Server running at http://localhost:${port}`);
+    });
+}
+
+startServer(port);
